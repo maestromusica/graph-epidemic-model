f6a4635da05ea3f0c1c60883eae170f666ef75ba
Łukasz Domański <luk.domanski@gmail.com>
2016-02-16 20:33:17 +0000

    Now they fucking die

--- d3_graph_gen.js
+++ d3_graph_gen.js
@@ -3,7 +3,7 @@
 var width = $(window).width(),
     height = $(window).height();
 
-	
+
 var color = d3.scale.category10();
 
 var force = d3.layout.force()
@@ -17,10 +17,10 @@ var force = d3.layout.force()
 		if (d.value==2) return 1.0;
 		else return 0.1;
 	});
-	
+
 
 var illNodesIndex = [];
-	
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -62,30 +62,59 @@ var svg = d3.select("body").append("svg")
   });
 	illNodesIndex = [];
 
-	svg.selectAll(".node").on('dblclick' , function(d){ 
+	svg.selectAll(".node").on('dblclick' , function(d){
 		d3.select("#name" + d.index).attr("class", "node-ill");
-	
+
 		illNodesIndex.push(d.index);
 });
 
 }
 
-var society = gen_society(5,2,100);
+var NUM_OF_NODES = 100;
+var society = gen_society(5,2,NUM_OF_NODES);
 f(false, society);
+
+// ok - healthy
+// ill - infected
+// dead - well...
+var nodes = [];
+
+for(var i=0; i<NUM_OF_NODES; i++) {
+  nodes[i] = "ok";
+}
 
 
 setInterval(function(){
-	var newIllNodes=illNodesIndex.slice();
-	for (var i=0;i<illNodesIndex.length;++i) {
-		//for every neighbour
-		var current = society.nodes[illNodesIndex[i]];
-		for (var j=0; j<current.links.length;++j) {
-			if (newIllNodes.indexOf(current.links[j])==-1&&Math.random()<0.1)
-				newIllNodes.push(current.links[j]);
-		}
-	}
-	for (var i=0;i<newIllNodes.length;++i)
-		d3.select("#name" + newIllNodes[i]).attr("class", "node-ill");
-	illNodesIndex=newIllNodes;
-}, 1000);
+  var newIllNodes = illNodesIndex.slice();
 
+  for (var i=0;i<illNodesIndex.length;i++) {
+    if(Math.random()<0.4 && nodes[i] == "ill") {
+      d3.select("#name" + newIllNodes[i]).style({opacity: 0.1});
+      nodes[i] == "dead";
+    }
+  }
+
+  for (var i=0;i<illNodesIndex.length;++i) {
+    if(nodes[i] == "dead" || illNodesIndex[i] == -1)
+      continue;
+
+    //for every neighbour
+    var current = society.nodes[illNodesIndex[i]];
+    for (var j=0; j<current.links.length;++j) {
+      if (newIllNodes.indexOf(current.links[j])==-1&&Math.random()<0.7&&nodes[j]=="ok") {
+        newIllNodes.push(current.links[j]);
+      }
+    }
+  }
+
+
+  for (var i=0;i<newIllNodes.length;++i) {
+    if(nodes[i]=="ok") {
+      d3.select("#name" + newIllNodes[i]).attr("class", "node-ill");
+      nodes[i] = "ill";
+    }
+  }
+  illNodesIndex=newIllNodes.slice();
+
+
+}, 1000);
